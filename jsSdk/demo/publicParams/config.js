4dd44d92ad3f183dbc85469c4b0d8868508c928f
liushuai <1402431596@qq.com>
2019-11-29 11:12:07 +0800

    test updateName

---
+++ jsSdk/demo/publicParams/config.js
@@ -0,0 +1,11 @@
+export const SECRET = '';   // 万店掌开放平台对应商第三方的开发者Secret
+// API 公共参数配置文件
+export const CONFIG = {
+    _aid: 'S107',                                // 开放平台系统编号
+    _akey: '',                      // 万店掌开放平台分配给第三方的开发者key
+    // _mt: '',                                  // 接口名称
+    _sm: 'md5',                                  // 签名算法 md5,sha1
+    // _requestMode: '',                         // 请求方式post,get
+    _version: 'v1',                              // 版本号
+    // _sig: ''                                  // 签名token
+};
